58208958cc4f6475890f99f4f75f98b5bd258840
Ryan Pliske <ryan.pliske@monsanto.com>
2015-12-30 11:02:00 -0600

    Initial Commit

---
+++ TrackerRP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrackerRP
+//
+//  Created by PLISKE, RYAN [AG-Contractor/1000] on 12/30/15.
+//  Copyright © 2015 Ryan Pliske. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
